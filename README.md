db822209a6378c71f2c4f465b5787223eb514923
ohmji <jirawatk1999@hotmail.com>
2019-08-12 09:44:15 +0700

    Revert "first commit"
    This reverts commit a7f588efe25b57db50d9256b30b7d0cbfc6e3405.

--- README.md
+++
@@ -1,1 +0,0 @@
-# reward-backend-express
--- config/passport.js
+++
@@ -1,9 +0,0 @@
-var passport = require('passport')
-
-
-module.exports = function()  {
-    passport.serializeUser(function(user, cb) {cb(null, user);});
-    passport.deserializeUser(function(obj, cb) {cb(null, obj);});
-
-    require('./lineAuth/LineAuth')();
-}
